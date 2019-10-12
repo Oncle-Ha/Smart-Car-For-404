1ab998fe787bd97ad61c98d882873134165bb5b1
Oncle-Ha <Herrwerner@163.com>
2019-10-12 11:10:48 +0800

    LXK0.10

--- kea/src/APP/isr.c
+++ kea/src/APP/isr.c
@@ -1,105 +1,106 @@
 #include "isr.h"
-
-
-//����0�����жϷ�������
+extern int *LED_state;
+//串口0接收中断服务例程
 void UART0_ISR(void)
-{    
-  DisableInterrupts ;//�����ж�
-  uint8_t Data='0';
-  if(UART0->S1 & UART_S1_RDRF_MASK)   //�������ݼĴ�����
-  {
-    Data=uart_getchar(UARTR0);
-    //...
-  }
-  if(UART0->S1  & UART_S1_TDRE_MASK )  //�������ݼĴ�����
-  {
-    uart_putchar(UARTR0,Data);
-    //...
-  }
-  EnableInterrupts;   //�����ж�
+{
+    DisableInterrupts; //关总中断
+    uint8_t Data = '0';
+    if (UART0->S1 & UART_S1_RDRF_MASK) //接收数据寄存器满
+    {
+        Data = uart_getchar(UARTR0);
+        if(Data == 's') {
+            PIT_IRQ_EN(PIT_CHANNEL0);
+        }
+        else if(Data == 'p'){ 
+            PIT_IRQ_DIS(PIT_CHANNEL0);
+        }
+    }
+    /* if (UART0->S1 & UART_S1_TDRE_MASK) //发送数据寄存器空
+    {
+        uart_putchar(UARTR0, Data);
+        //...
+    }
+    */
+    EnableInterrupts; //开总中断
 }
 
-
-//����1�����жϷ�������
+//串口1接收中断服务例程
 void UART1_ISR(void)
-{    
-  DisableInterrupts ;//�����ж�
-  uint8_t Data='1';
-  if(UART1->S1 & UART_S1_RDRF_MASK)   //�������ݼĴ�����
-  {
-    Data=uart_getchar(UARTR1);
-    //...
-  }
-  if(UART1->S1  & UART_S1_TDRE_MASK )  //�������ݼĴ�����
-  {
-    uart_putchar(UARTR1,Data);
-    //...
-  }
-  EnableInterrupts;   //�����ж�
+{
+    DisableInterrupts; //关总中断
+    uint8_t Data = '1';
+    if (UART1->S1 & UART_S1_RDRF_MASK) //接收数据寄存器满
+    {
+        Data = uart_getchar(UARTR1);
+        //...
+    }
+    if (UART1->S1 & UART_S1_TDRE_MASK) //发送数据寄存器空
+    {
+        uart_putchar(UARTR1, Data);
+        //...
+    }
+    EnableInterrupts; //开总中断
 }
 
-//����2�����жϷ�������
+//串口2接收中断服务例程
 void UART2_ISR(void)
-{    
-  DisableInterrupts ;//�����ж�
-  uint8_t Data='2';
-  if(UART2->S1 & UART_S1_RDRF_MASK)   //�������ݼĴ�����
-  {
-    Data=uart_getchar(UARTR2);
-    //...
-  }
-  if(UART2->S1  & UART_S1_TDRE_MASK )  //�������ݼĴ�����
-  {
-    uart_putchar(UARTR2,Data);
-    //...
-  }
-  EnableInterrupts;   //�����ж�
+{
+    DisableInterrupts; //关总中断
+    uint8_t Data = '2';
+    if (UART2->S1 & UART_S1_RDRF_MASK) //接收数据寄存器满
+    {
+        Data = uart_getchar(UARTR2);
+        //...
+    }
+    if (UART2->S1 & UART_S1_TDRE_MASK) //发送数据寄存器空
+    {
+        uart_putchar(UARTR2, Data);
+        //...
+    }
+    EnableInterrupts; //开总中断
 }
 
-
-
-int stime = 0;  //ϵͳʱ��
-//��ʱ��0�жϺ���
+int stime = 0; //系统时间
+//定时器0中断函数
 
 void PIT0_ISR(void)
 {
-  PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;//����жϱ�־λ
+    PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK; //清除中断标志位
 
-  stime++;  
-  uartPrintf(UARTR2,"hello %d\n",stime);
-  
+    // stime++;
+    // uartPrintf(UARTR2,"hello %d\n",stime);
+    int temp = LED_state[0];
+    LED_state[0] = LED_state[3];
+    LED_state[3] = LED_state[2];
+    LED_state[2] = LED_state[1];
+    LED_state[1] = temp;
 }
 
-
-
-
 /*
-Ӧ����PTA0-PTD7���ⲿ�ж�
+应用于PTA0-PTD7的外部中断
 
 */
-//KBI0�жϺ���
-void KBI0_Isr(void)	
+//KBI0中断函数
+void KBI0_Isr(void)
 {
-  
-  KBI0->SC |= KBI_SC_KBACK_MASK;       /* clear interrupt flag */
-  KBI0->SC |= KBI_SC_RSTKBSP_MASK ;    //����жϱ�־λ
-  
-  if(!gpio_get(PTD5)) // �ж�PTD5�Ƿ��ǵ͵�ƽ
-  {
-    uartPrintf(UARTR2,"PTD5 interrupt\n");
-  }
-  
+
+    KBI0->SC |= KBI_SC_KBACK_MASK;   /* clear interrupt flag */
+    KBI0->SC |= KBI_SC_RSTKBSP_MASK; //清除中断标志位
+
+    if (!gpio_get(PTD5)) // 判断PTD5是否是低电平
+    {
+        uartPrintf(UARTR2, "PTD5 interrupt\n");
+    }
 }
 
-
 /*
-Ӧ����PTE0-PTH7���ⲿ�ж�
+应用于PTE0-PTH7的外部中断
 
 */
-//KBI1�жϺ���
-void KBI1_Isr(void)	
+//KBI1中断函数
+void KBI1_Isr(void)
 {
-  
-  KBI1->SC |= KBI_SC_KBACK_MASK;                /* clear interrupt flag */
-  KBI1->SC |= KBI_SC_RSTKBSP_MASK ;             //����жϱ�־λ
+
+    KBI1->SC |= KBI_SC_KBACK_MASK;   /* clear interrupt flag */
+    KBI1->SC |= KBI_SC_RSTKBSP_MASK; //清除中断标志位
 }
